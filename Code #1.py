9344f8c7bc639a2f2fdcb8bdfb19a7f2cb9e3825
SebastianBMontefalcon <sebastianmel.montefalcon@smc.pshs.edu.ph>
2025-09-15 19:27:16 +0800

    Delete Code #1.py

--- Code #1.py
+++
@@ -1,5 +0,0 @@
-num1 = float(input("Enter the first number:"))
-num2 = float(input("Enter the second number:"))
-num3 = float(input("Enter the third number:"))
-sum = num1 + num2 + num3
-print("The total of the three numbers is:",round(sum, 2))
